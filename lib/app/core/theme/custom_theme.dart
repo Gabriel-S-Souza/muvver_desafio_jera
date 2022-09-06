fe36e730ed268a51dc5899a0fc0e0aa1d4e18924
Gabriel Souza <gabriel12saraiva@gmail.com>
2022-09-05 23:07:35 -0300

    Creates a basic theme

--- lib/app/core/theme/custom_theme.dart
+++ lib/app/core/theme/custom_theme.dart
@@ -3,98 +3,67 @@ import 'package:flutter/material.dart';
 class CustomTheme {
   static ThemeData theme = ThemeData(
       useMaterial3: true,
-      scaffoldBackgroundColor: CustomColors.backgroundLight,
+      scaffoldBackgroundColor: CustomColors.white,
       colorScheme: const ColorScheme(
         brightness: Brightness.light,
-        primary: CustomColors.primary,
-        onPrimary: CustomColors.primaryDark,
-        secondary: CustomColors.backgroundLight,
-        onSecondary: CustomColors.backgroundDark,
+        primary: CustomColors.green,
+        onPrimary: CustomColors.green,
+        secondary: CustomColors.black,
+        onSecondary: CustomColors.black,
         error: CustomColors.error,
         onError: CustomColors.error,
-        background: CustomColors.backgroundLight,
-        onBackground: CustomColors.backgroundLightMedium,
-        surface: CustomColors.backgroundDarkMedium,
-        onSurface: CustomColors.backgroundDark,
-        onSurfaceVariant: CustomColors.backgroundContainerDark,
-        tertiary: CustomColors.white,
-        surfaceVariant: CustomColors.accent,
-        shadow: CustomColors.blackLight,
-        inversePrimary: CustomColors.black,
-        onPrimaryContainer: CustomColors.grey,
-        onSecondaryContainer: CustomColors.greyLight,
-        inverseSurface: CustomColors.lightBackground,
+        background: CustomColors.white,
+        onBackground: CustomColors.white,
+        surface: CustomColors.greenLight,
+        onSurface: CustomColors.greenLight,
+        onTertiary: CustomColors.greenLight,
+        outline: CustomColors.black,
+        tertiary: CustomColors.gray,
+        surfaceTint: CustomColors.greenLight
       ),
-      textSelectionTheme: const TextSelectionThemeData(
-        cursorColor: CustomColors.blackLightMedium,
-        selectionColor: CustomColors.blackLightMedium,
-        selectionHandleColor: CustomColors.white,
-      ),
-      fontFamily: 'Montserrat',
-      scrollbarTheme: ScrollbarThemeData(
-        thumbColor:
-            MaterialStateProperty.all(CustomColors.grey.withOpacity(0.6)),
-      ),
+      fontFamily: 'TitilliumWeb',
       textTheme: const TextTheme(
         headline1: TextStyle(
-          fontSize: 50,
-          fontWeight: FontWeight.bold,
+          fontSize: 32,
+          fontWeight: FontWeight.w400,
         ),
         headline2: TextStyle(
-          fontSize: 42,
-          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
         ),
         headline3: TextStyle(
-          fontSize: 34,
-          fontWeight: FontWeight.bold,
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
         ),
         headline4: TextStyle(
-          fontSize: 28,
-          fontWeight: FontWeight.bold,
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
         ),
         headline5: TextStyle(
-          fontSize: 26,
-          fontWeight: FontWeight.bold,
-        ),
-        headline6: TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.bold,
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
         ),
         bodyText1: TextStyle(
           fontSize: 16,
-          fontFamily: 'Calibri',
-          fontWeight: FontWeight.w800,
-        ),
-        bodyText2: TextStyle(
-          fontSize: 12,
-          fontFamily: 'Calibri',
           fontWeight: FontWeight.w800,
         ),
         subtitle1: TextStyle(
           fontSize: 14,
-          color: CustomColors.white,
-          fontWeight: FontWeight.bold,
+          fontWeight: FontWeight.w400,
+        ),
+        bodyText2: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w800,
         ),
       ));
 }
 
 class CustomColors {
-  static const Color primary = Color(0xFFFFAB00);
-  static const Color primaryDark = Color(0xFFDE9C2E);
   static const Color white = Color(0xFFFFFFFF);
-  static const Color backgroundLight = Color(0xFF00A2AC);
-  static const Color backgroundLightMedium = Color(0xFF018189);
-  static const Color backgroundDarkMedium = Color(0xFF016A71);
-  static const Color backgroundDark = Color(0xFF00585D);
-  static const Color backgroundContainerDark = Color(0xFF0D575D);
-  static const Color error = Color(0xFFB00020);
-  static const Color splashColor = Color(0xFF0FB2AA);
-  static const Color accent = Color(0xFF80D1D6);
-
-  static const Color black = Colors.black;
-  static const Color blackLightMedium = Colors.black54;
-  static const Color blackLight = Colors.black38;
-  static const Color grey = Color(0xFF6F6F6F);
-  static const Color greyLight = Color(0xFFA0A0A0);
-  static const Color lightBackground = Color(0xFFF5F7F6);
+  static const Color green = Color(0xFF16A45C);
+  static const Color greenLight = Color(0xFF24B96E);
+  static const Color gray = Color(0xFFB8B8B8);
+  static const Color black = Color(0xFF222222);
+  static const Color blackLight = Color(0xFF353740);
+  static const Color error = Colors.red;
 }
